8eff1f93f68fa4c3bca65174847122e8c161eef9
Seyoung Cho <simplistudiodev@gmail.com>
2023-05-09 12:50:22 +0900

    - Add buildSrc - Replace build.gradle to build.gradle.kts - Configured versions on buildSrc

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    id("com.android.application").version(AppVersions.application).apply(false)
+    id("com.android.library").version(AppVersions.library).apply(false)
+    id("org.jetbrains.kotlin.android").version(AppVersions.kotlinAndroid).apply(false)
+}
---
+++ buildSrc/src/main/kotlin/version/AppVersions.kt
@@ -0,0 +1,6 @@
+
+object AppVersions {
+    const val application = "8.0.1"
+    const val library = "8.0.1"
+    const val kotlinAndroid = "1.8.10"
+}
