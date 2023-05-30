820341749c80cdb951d31d0c104678df96f2f8c4
seyoungcho2 <seyoungcho2@gmail.com>
2023-05-31 00:46:51 +0900

    Change Default Color for Material2

--- dynamic-theme/src/main/java/io/github/seyoungcho2/dynamictheme/model/ColorPalette.kt
+++ dynamic-theme/src/main/java/io/github/seyoungcho2/dynamictheme/model/ColorPalette.kt
@@ -1,13 +1,15 @@
 package io.github.seyoungcho2.dynamictheme.model
 
 import androidx.compose.material.Colors
+import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 
 data class ColorPalette(
     val lightModeColors: Colors = DefaultLightColorPalette,
-    val darkModeColors: Colors = lightModeColors
+    val darkModeColors: Colors = DefaultDarkColorPalette
 ) {
     companion object {
         internal val DefaultLightColorPalette: Colors = lightColors()
+        internal val DefaultDarkColorPalette: Colors = darkColors()
     }
 }
